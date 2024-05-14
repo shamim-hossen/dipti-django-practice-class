e38ba2f9746e09e4eea246354db28e6af9c09750
Shamim Hossen <shamimrejone@gmail.com>
2024-05-14 14:00:07 +0600

    add class test file

--- bkash-ussd-app-using-if-else/test.py
+++ bkash-ussd-app-using-if-else/test.py
@@ -1,3 +1,106 @@
 # print(1-100 with increasing 10 in every step)
 for i in range(0,101,10):
     print(i)
+
+    # Even print with while loop start with 0
+# i=0
+# while i<=10:
+#     print(i)
+#     i+=2
+# print('outer loop')
+
+
+# Even print with while loop start with 1
+# i=1
+# while i<=10:
+#     if i==1:
+#         i+=1
+#     print(i)
+#     i+=2
+# print('outer loop')
+
+
+# while loop
+# i=1
+# while i<10:
+#     if i==3 or i==6 or i==8:
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+# print("outer")
+
+
+
+
+# while loop
+# print("Natural number from",n,"-1 in reverse")
+# n=int(input("Input n:"))
+# print(f"Natural number from {n}-1 in reverse")
+# while n>0:
+#     if n==1:
+#         print(n, end='')
+#     else:
+#         print(n, end=',')
+#     n-=1
+
+# i=1
+# while i<6:
+#     print(i, end=' ')
+#     i+=1
+# else:
+#     print("i is no longer less than 6")
+
+# tlist=['apple','banana','cherry']
+# i=0
+# while i < len(tlist):
+#     print(tlist[i])
+#     i+=1
+
+
+# tlist=['apple','banana','cherry']
+# for i in tlist:
+#     print(i)
+
+
+
+# str = "banana"
+# for i in str:
+#     print(i, end='\n')
+# else:
+#     print("end the loop")
+
+
+
+# str = "bananajsdafjsdf"
+# for i in str:
+#     if i == 'j':
+#         break
+#     print(i, end=' ')
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for i in fruits:
+#     if i == "banana":
+#         print(i)
+#         break
+
+
+# fruits = [['r', 'n','h','s'], "banana", "cherry"]
+# for x in fruits:
+#     for i in x:
+#         print(i)
+#     print("\n")
+
+
+
+# fruits = [['ranajit', 'naiyem', 'hannan', 'sagor'], "banana", "cherry"]
+# for item in fruits[0]:
+#     for i in item:
+#         for j in i:
+#             print(j)
+#         print('\n')
+
+
+for i in range(0,61,10):
+    print(i)
