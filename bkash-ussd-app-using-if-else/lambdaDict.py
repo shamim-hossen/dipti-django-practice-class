507f4a46cd28ef782d2440087b99064837b9caed
Shamim Hossen <shamimrejone@gmail.com>
2024-05-23 22:31:13 +0600

    add lambda

---
+++ bkash-ussd-app-using-if-else/lambdaDict.py
@@ -0,0 +1,88 @@
+
+
+'''lamda function :
+    1.a small anonymous function
+    2.take any number of arguments, but can only have one expression.
+'''
+# x = lambda a: a+10
+# print(x(5))
+
+
+# x = lambda a,b,c,d,e: a+b+c+d+e
+# print(x(1,2,3,4,5))
+
+
+# x = lambda a,b,c,d,e: a-b-c-d-e
+# print(x(1,2,3,4,5))
+
+
+
+
+
+
+# def myfunc(n):
+#     return lambda a: a * n
+
+
+# mydoubler = myfunc(2)
+# mytripler = myfunc(3)
+
+
+# print(mydoubler(11))
+# print(mytripler(11))
+
+
+
+
+
+
+# def myfunc(a,b,c):
+#     return lambda x,y,z: a + b + c + x + y + z
+
+
+# mydoubler = myfunc(1,2,3)
+# result = mydoubler(4,5,6)
+# print(result)
+
+
+'''Touple find 2nd index of 8 '''
+# thistuple = (1, 3, 7, 8, 7, 5, 8, 5,6,9,8,2,7)
+# count = 0
+# i = 0
+# for x in thistuple:
+#     if x==8:
+#         i+=1
+#     if x==8 and i==3:
+#         print(count)
+#     count+=1
+
+
+'''Dictionary'''
+# thisdictionary = {
+#     "Name": "Jhon",
+#     "Age": "20",
+#     "Country": "Norway",
+# }
+# print(thisdictionary['Name'])
+
+
+'''Convert to dictionary'''
+# thisdict = dict(name = "Jhon", age = 36, country = "Norway")
+# print(thisdict)
+
+
+
+
+# thisdict = {
+#     "brand": "ford",
+#     "model": "Musture",
+#     "year": "1997",
+# }
+# x = thisdict.keys()
+# for i in x:
+#     print(thisdict[i])
+
+
+
+
+
